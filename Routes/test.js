91e4d6b1d3b8823306c4b553a9483fe12a3c4de7
ApichaiTi <81065469+artijo@users.noreply.github.com>
2024-09-20 14:53:33 +0700

    middleware

--- Routes/test.js
+++ Routes/test.js
@@ -1,10 +1,15 @@
 import express from 'express';
 
+// Import the controller
 import { TestController } from '../Controllers/testController.js';
+
+// import the middleware
+import { testmiddleware } from '../Middlewares/testMiddleware.js';
+
 
 const router = express.Router();
 
-
-router.get('/', TestController);
+// Use middleware and controller
+router.get('/', testmiddleware ,TestController);
 
 export default router;
