8d645360d65f5cc69f9ea1f7605692414edfa369
ApichaiTi <81065469+artijo@users.noreply.github.com>
2024-09-24 15:59:05 +0700

    test insert data

--- Controllers/testController.js
+++ Controllers/testController.js
@@ -1,5 +1,31 @@
+const userSchema = require('../Models/User');
+
 function TestController(req, res) {
     res.send('Hello Test!');
 }
+function InsertUser(req, res) {
+    const newUser = new userSchema({
+        username: "test",
+        password: "test",
+        email: "t@test.com",
+        name: "test",
+        birthdate: new Date(),
+        tel: "123456789"
+    });
+    newUser.save().then((result) => {
+        res.send(result);
+    }).catch((error) => {
+        res.send(error);
+    });
+}
 
-module.exports = { TestController };
+function getAllUsers(req, res) {
+    userSchema.find().then((result) => {
+        res.send(result);
+    }).catch((error) => {
+        res.send(error);
+    }
+    );
+}
+
+module.exports = { TestController, InsertUser, getAllUsers };
